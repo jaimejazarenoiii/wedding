7e534e1f0abf8b2a2a2f2bd0be57b8bb78301433
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2019-07-27 01:47:07 +0800

    Create loader view

--- assets/javascripts/action.js
+++ assets/javascripts/action.js
@@ -1,22 +1,34 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamFpbWVjaHVyIiwiYSI6ImNqc3h2eGxjMTBxNnQ0M3MxaWFidG04Z3QifQ.aczFGO6YS18EDic757AHrA';
-var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
-  center: [121.098523, 14.632511], // starting position
-  zoom: 16// starting zoom
-});
-var map2 = new mapboxgl.Map({
-  container: 'map2', // container id
-  style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
-  center: [121.111877, 14.649495], // starting position
-  zoom: 16 // starting zoom
-});
+window.onload = function () {
+  start()
+  var hiddenDiv = document.querySelector('.is-hidden')
+  var displayedDiv = document.querySelector('.is-overlay')
 
-var marker = new mapboxgl.Marker({color: "#B9A481"})
-.setLngLat([121.098523, 14.632511]);
-var marker2 = new mapboxgl.Marker({color: "#B9A481"})
-.setLngLat([121.111877, 14.649495]);
+  displayedDiv.classList.add('animated', 'fadeOut', 'delay-4s')
+  var delayInMilliseconds = 1000; //1 second
 
-marker.addTo(map)
-marker2.addTo(map2)
+  setTimeout(function() {
+    hiddenDiv.classList.remove('is-hidden')
+    hiddenDiv.classList.add('animated', 'fadeIn')
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamFpbWVjaHVyIiwiYSI6ImNqc3h2eGxjMTBxNnQ0M3MxaWFidG04Z3QifQ.aczFGO6YS18EDic757AHrA';
+    var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
+      center: [121.098523, 14.632511], // starting position
+      zoom: 16// starting zoom
+    });
+    var map2 = new mapboxgl.Map({
+      container: 'map2', // container id
+      style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
+      center: [121.111877, 14.649495], // starting position
+      zoom: 16 // starting zoom
+    });
 
+    var marker = new mapboxgl.Marker({color: "#B9A481"})
+      .setLngLat([121.098523, 14.632511]);
+    var marker2 = new mapboxgl.Marker({color: "#B9A481"})
+      .setLngLat([121.111877, 14.649495]);
+
+    marker.addTo(map)
+    marker2.addTo(map2)
+  }, delayInMilliseconds);
+}
